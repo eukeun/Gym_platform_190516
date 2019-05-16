1c1b22d3ad44176d2a73976e7ee0a31018b73a84
eukeun <g9016@naver.com>
2019-05-16 23:12:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/gym_platform/PayActivity.java
@@ -0,0 +1,163 @@
+package com.example.gym_platform;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PayActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+
+        TextView pay_100 = (TextView)findViewById(R.id.TextView_Point_100);
+        TextView pay_250 = (TextView)findViewById(R.id.TextView_Point_250);
+        TextView pay_500 = (TextView)findViewById(R.id.TextView_Point_500);
+        TextView pay_1000 = (TextView)findViewById(R.id.TextView_Point_1000);
+        TextView pay_2500 = (TextView)findViewById(R.id.TextView_Point_2500);
+        TextView pay_5000 = (TextView)findViewById(R.id.TextView_Point_5000);
+
+        pay_100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(100);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+        pay_250.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(250);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+        pay_500.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(500);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+        pay_1000.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(1000);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+        pay_2500.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(2500);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+        pay_5000.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePoint(5000);
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Intent intent = new Intent(PayActivity.this, NavigationActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },1000);
+            }
+        });
+    }
+
+    public void updatePoint(final int point) {
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        DocumentReference docRef = db.collection("User").document(currentUser.getUid());
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document= task.getResult();
+                    if (document.exists()) {
+                        StringBuilder fields = new StringBuilder("");
+
+                        Map<String, Object> User = new HashMap<>();
+                        User.put("userPoint", point+Integer.parseInt(document.get("userPoint").toString()));
+                        db.collection("User").document(currentUser.getUid())
+                                .update(User)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                    }
+                                });
+                    } else {
+                    }
+                } else {
+                }
+            }
+        });
+    }
+}
