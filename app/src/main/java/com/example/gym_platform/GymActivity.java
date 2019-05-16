0d2da981e5b50c5f49b608adcb149153925478c1
eukeun <g9016@naver.com>
2019-05-16 23:55:44 +0900

    add trainer

--- app/src/main/java/com/example/gym_platform/GymActivity.java
+++ app/src/main/java/com/example/gym_platform/GymActivity.java
@@ -68,22 +68,22 @@ public class GymActivity extends AppCompatActivity {
         fragmentAdapter1.notifyDataSetChanged();
 
 
-        TabHost tab_host = (TabHost) findViewById(R.id.tabhost);
+        TabHost tab_host = (TabHost) findViewById(R.id.gtabhost);
         tab_host.setup();
 
-        TabHost.TabSpec ts1 = tab_host.newTabSpec("tab1");
+        TabHost.TabSpec ts1 = tab_host.newTabSpec("gtab1");
         ts1.setIndicator("상품");
-        ts1.setContent(R.id.tab1);
+        ts1.setContent(R.id.gtab1);
         tab_host.addTab(ts1);
 
-        TabHost.TabSpec ts2 = tab_host.newTabSpec("tab2");
+        TabHost.TabSpec ts2 = tab_host.newTabSpec("gtab2");
         ts2.setIndicator("정보");
-        ts2.setContent(R.id.tab2);
+        ts2.setContent(R.id.gtab2);
         tab_host.addTab(ts2);
 
-        TabHost.TabSpec ts3 = tab_host.newTabSpec("tab3");
+        TabHost.TabSpec ts3 = tab_host.newTabSpec("gtab3");
         ts3.setIndicator("리뷰");
-        ts3.setContent(R.id.tab3);
+        ts3.setContent(R.id.gtab3);
         tab_host.addTab(ts3);
 
         tab_host.setCurrentTab(0);
